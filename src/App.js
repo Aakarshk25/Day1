d8f389d34d27ebe19d5f9841c54a1ecbd7e6df07
Aakarsh Srivastava <132938129+Aakarshk25@users.noreply.github.com>
2023-07-03 16:48:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import {BrowseRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { Box } from '@mui/material';
-import Navbar from './components/Navbar';
-import { Feed } from '@mui/icons-material';
-import VideoDetail from './components/VideoDetail';
-import SearchFeed from './components/SearchFeed';
-import ChannelDetail from './components/ChannelDetail';
 
-
-
-
-const App = () =>
-  (
-    <BrowseRouter>
-    <Box sx={{backgroundColor: '#000'}}>
-        <Navbar />
-        <Routes>
-            <Route path='/' element={<Feed />} />
-            <Route path='/vedio/:id' element={<VideoDetail />} />
-            <Route path='/channel/:id' element={<ChannelDetail />} />
-            <Route path='/search/:searchTerm' element={<SearchFeed />} />
-        </Routes>
+import {Navbar, Feed,VideoDetail, ChannelDetail,SearchFeed} from './components';
+const App = () => (
+  <Router>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
     </Box>
-    </BrowseRouter>
-  );
+  </Router>
+);
 
 
 
-export default App
+export default App;
