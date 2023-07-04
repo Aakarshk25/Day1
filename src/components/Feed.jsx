b237877e513025c950cd60e2a1df3fd15229532f
Aakarsh Srivastava <132938129+Aakarshk25@users.noreply.github.com>
2023-07-04 16:41:53 +0530

    Updated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Feed = () => {
   return (
-    <div>feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+        slidebar
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
+          copyright 2023 Akasi
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+import { Search } from '@mui/icons-material';
+
+const SearchBar = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+  };
+
+  const handleChange = (event) => {
+    // Handle input change logic here
+  };
+
+  return (
+    <Paper
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 }
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="search..."
+        value=""
+        onChange={handleChange}
+      />
+      <IconButton type="submit" sx={{ p: '10px', color: 'red' }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
